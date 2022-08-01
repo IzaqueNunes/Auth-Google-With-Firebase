8e488496282eaaf017c31823d7b934e407eab7dd
IzaqueNunes <izaquenunes560@gmail.com>
2022-08-01 12:13:39 -0300

    estrutura basica

---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,22 @@
+import { GoogleLogo } from "phosphor-react";
+import "./styles.scss";
+
+export function SignIn() {
+  return (
+    <div className="container">
+      <h1>Acesse sua conta</h1>
+
+      <span>
+        Utilizando autenticação social, por exemplo, a autenticação com a
+        Google, você <br />
+        facilita a vida do usuário permitindo utilizar sua aplicação sem fazer
+        cadastro.
+      </span>
+
+      <button type="button" className="button">
+        <GoogleLogo />
+        Entrar com Google
+      </button>
+    </div>
+  );
+}
